6dd78e65a59d785da19b243c387facd5c24bb35b
wmizukami <user@Mizukami.local>
2015-06-24 13:17:57 +0900

    クラス名修正

--- lib/login_attack_report/frameworks/active_record/models/l_a_r_version.rb
+++ lib/login_attack_report/frameworks/active_record/models/l_a_r_version.rb
@@ -1,7 +1,7 @@
 require 'login_attack_report/l_a_r_version_concern'
 
 module LoginAttackReport
-  class LAReportVersion < ::ActiveRecord::Base
+  class LARVersion < ::ActiveRecord::Base
     include LoginAttackReport::LARVersionConcern
   end
 end
--- lib/login_attack_report/version.rb
+++ lib/login_attack_report/version.rb
@@ -1,3 +1,3 @@
 module LoginAttackReport
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
